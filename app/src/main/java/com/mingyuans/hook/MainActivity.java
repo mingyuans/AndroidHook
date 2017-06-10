f2d3e27b51065e36514b6e233f182dffcd55e8a3
yanxq <yanxiangqunwork@163.com>
2017-06-10 21:23:24 +0800

    修改 elfhook 名称

--- app/src/main/java/com/mingyuans/hook/MainActivity.java
+++ app/src/main/java/com/mingyuans/hook/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
-        System.loadLibrary("elfhook-lib");
+        System.loadLibrary("elfhook");
     }
 
     @Override
